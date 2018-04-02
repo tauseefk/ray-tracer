ef010ff8cde01f133cfc0e95fcd51a36f9109033
Tauseef Khan <tauseef25@gmail.com>
2018-04-01 18:34:21 -0700

    abstract classes for hitable objects and camera

--- main.cpp
+++ main.cpp
@@ -1,36 +1,19 @@
-#include "ray.h"
-#include "vec3.h"
+#include "camera.h"
+#include "hitable_list.h"
+#include "sphere.h"
 #include <iostream>
 
-/**
- * Dot product of a ray from a point on the sphere to it's center would be the
- * square of the circle's radius dot.
- * ((A-C + t*B), (A-C + t*B)) - R*R = 0;
- * t*t*dot(B, B) + 2*t*dot((A-C), B) + dot ((A-C), (A-C)) - R*R = 0;
- */
-float hit_sphere(const ray& r, const vec3& center, float radius) {
+vec3 color(ray& r, hitable* world) {
+  hit_record rec;
 
-  vec3 oc = r.origin() - center;               // A - C;
-  float a = dot(r.direction(), r.direction()); // dot(B, B);
-  float b = 2 * dot(oc, r.direction());        // dot(A-C, B);
-  float c = dot(oc, oc) - radius * radius;     // dot ((A-C), (A-C)) - R*R;
-  float discriminant = b * b - (4 * a * c);
-  if (discriminant < 0) {
-    return -1.0;
+  if (world->hit(r, 0.0, MAXFLOAT, rec)) {
+    return 0.5 *
+           vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
   } else {
-    return (-b - sqrt(discriminant)) / (2.0 * a);
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
   }
-}
-
-vec3 color(ray& r) {
-  float t = hit_sphere(r, vec3(0, 0, -1), 0.5);
-  if (t > 0.0) {
-    vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
-    return 0.5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
-  }
-  vec3 unit_direction = unit_vector(r.direction());
-  t = 0.5 * (unit_direction.y() + 1.0);
-  return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
 int main() {
@@ -40,17 +23,19 @@ int main() {
             << nx << " " << ny << std::endl
             << "255" << std::endl;
 
-  vec3 lower_left_corner(-2.0, -1.0, -1.0);
-  vec3 horizontal(4.0, 0.0, 0.0);
-  vec3 vertical(0.0, 2.0, 0.0);
-  vec3 origin(0.0, 0.0, 0.0);
+  hitable* list[3];
+  list[0] = new sphere(vec3(0, 0, -1), 0.5);
+  list[1] = new sphere(vec3(0, 1, -1), 0.5);
+  list[2] = new sphere(vec3(0, -100.5, -1), 100);
+  hitable* world = new hitable_list(list, 3);
+  camera cam;
 
   for (int j = ny - 1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
       float u = float(i) / float(nx);
       float v = float(j) / float(ny);
-      ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-      vec3 col = color(r);
+      ray r = cam.get_ray(u, v);
+      vec3 col = color(r, world);
       int ir = int(255.99 * col[0]);
       int ig = int(255.99 * col[1]);
       int ib = int(255.99 * col[2]);
---
+++ sphere.h
@@ -0,0 +1,47 @@
+#ifndef SPHEREH
+#define SPHEREH
+#include "hitable.h"
+
+class sphere : public hitable {
+public:
+  sphere() {}
+  sphere(vec3 cen, float r) : center(cen), radius(r) {}
+  virtual bool hit(const ray& r, float t_min, float t_max,
+                   hit_record& rec) const;
+  vec3 center;
+  float radius;
+};
+
+/**
+ * Dot product of a ray from a point on the sphere to it's center would be the
+ * square of the circle's radius dot.
+ * ((A-C + t*B), (A-C + t*B)) - R*R = 0;
+ * t*t*dot(B, B) + 2*t*dot((A-C), B) + dot ((A-C), (A-C)) - R*R = 0;
+ */
+
+bool sphere::hit(const ray& r, float t_min, float t_max,
+                 hit_record& rec) const {
+  vec3 oc = r.origin() - center;               // A - C;
+  float a = dot(r.direction(), r.direction()); // dot(B, B);
+  float b = dot(oc, r.direction());            // dot(A-C, B);
+  float c = dot(oc, oc) - radius * radius;     // dot ((A-C), (A-C)) - R*R;
+  float discriminant = b * b - (a * c);
+  if (discriminant > 0) {
+    float temp = (-b - sqrt(discriminant)) / (a);
+    if (temp < t_max && temp > t_min) {
+      rec.t = temp;
+      rec.p = r.point_at_parameter(rec.t);
+      rec.normal = (rec.p - center) / radius;
+      return true;
+    }
+    temp = (-b + sqrt(discriminant)) / a;
+    if (temp < t_max && temp > t_min) {
+      rec.t = temp;
+      rec.p = r.point_at_parameter(rec.t);
+      rec.normal = (rec.p - center) / radius;
+      return true;
+    }
+  }
+  return false;
+}
+#endif
