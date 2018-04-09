4e208f13899e6c7c6e904726da2daeb17132ccc9
Tauseef Khan <tauseef25@gmail.com>
2018-04-08 19:26:02 -0700

    multi-threading; resolution is a function of the number of threads, don't feel like fixing it

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include "material.h"
 #include "sphere.h"
 #include <iostream>
+#include <sstream>
+#include <thread>
+#include <vector>
 
 // XXX:TODO format is off
 vec3 color(const ray& r, hitable* world, int depth) {
@@ -23,9 +26,42 @@ vec3 color(const ray& r, hitable* world, int depth) {
   }
 }
 
+void cal_img_part(const int p, const int parts, const int nx, const int ny,
+                  const int ns, hitable* world, camera cam, float ray_v,
+                  std::string* img_parts) {
+
+  std::stringstream s;
+  for (int j = ((p + 1) * (ny / parts)) - 1; j >= p * (ny / parts); j--) {
+    for (int i = 0; i < nx; i++) {
+      vec3 col(0, 0, 0);
+      for (int s = 0; s < ns; s++) {
+        float u = float(i + drand48()) / float(nx);
+        float v = float(ray_v + drand48()) /
+                  float(ny); // we can use j instead of ray_v in case of
+                             // execution in a single chunk
+        ray r = cam.get_ray(u, v);
+        vec3 p = r.point_at_parameter(2.0);
+        col += color(r, world, 0);
+      }
+      col /= float(ns);
+      col = vec3(sqrt(col[0]), sqrt(col[1]),
+                 sqrt(col[2])); // gamma correction; raise to 1/gamma;
+      int ir = int(255.99 * col[0]);
+      int ig = int(255.99 * col[1]);
+      int ib = int(255.99 * col[2]);
+
+      s << ir << " " << ig << " " << ib << std::endl;
+    }
+    if (ray_v > 0) {
+      ray_v--;
+    }
+  }
+  img_parts[p] = s.str();
+}
+
 int main() {
-  int nx = 400;
-  int ny = 200;
+  int nx = 600;
+  int ny = 300;
   int ns = 100;
   std::cout << "P3" << std::endl
             << nx << " " << ny << std::endl
@@ -47,29 +83,34 @@ int main() {
   vec3 lookat(0, 0, -1);
   camera cam(lookfrom, lookat, vec3(0, 1, 0), 60, float(nx) / float(ny),
              float(1.0 / 8.0), (lookfrom - lookat).length());
-  
-  // std::string output[nx * ny];
-  // for(int p = 0; p < 2; p++){
-  //   std::string row;
 
-  // }
-  for (int j = ny - 1; j >= 0; j--) {
-    for (int i = 0; i < nx; i++) {
-      vec3 col(0, 0, 0);
-      for (int s = 0; s < ns; s++) {
-        float u = float(i + drand48()) / float(nx);
-        float v = float(j + drand48()) / float(ny);
-        ray r = cam.get_ray(u, v);
-        vec3 p = r.point_at_parameter(2.0);
-        col += color(r, world, 0);
-      }
-      col /= float(ns);
-      col = vec3(sqrt(col[0]), sqrt(col[1]),
-                 sqrt(col[2])); // gamma correction; raise to 1/gamma;
-      int ir = int(255.99 * col[0]);
-      int ig = int(255.99 * col[1]);
-      int ib = int(255.99 * col[2]);
-      std::cout << ir << " " << ig << " " << ib << std::endl;
-    }
+  // time_t start, end;
+  // time(&start);
+
+  const int parts = 3; // template argument can't reference unless
+                       // it's a const length array
+
+  std::string img_parts[parts];
+  float ray_v = ny - 1; // we need the rays to be consistent despite breaking
+                        // the work into several chunks
+
+  std::vector<std::thread> th;
+
+  // XXX:TODO create a thread pool and assign parts instead of deciding upfront
+  for (int i = 0; i < parts;
+       i++) { // can't spin the max number of threads coz the
+              // main process is gonna use one
+    th.push_back(std::thread(cal_img_part, i, parts, nx, ny, ns, world, cam,
+                             ((i + 1) * ny / parts) - 1, std::ref(img_parts)));
   }
+
+  for (auto& t : th) {
+    t.join();
+  }
+
+  for (int i = 0; i < parts; i++) {
+    std::cout << img_parts[parts - i - 1];
+  }
+  // time(&end);
+  // std::cout << difftime(end, start) << " seconds" << std::endl;
 }
